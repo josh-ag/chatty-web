04962db45e118d6e7214dd7ffe0d63c00575ecb6
gems <tech.gems.biz@gmail.com>
2022-12-02 19:41:29 +0100

    important update

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,4 @@
 import io from "socket.io-client";
+import { API_BASE_URL } from "./features/services/queries";
 
-export const socket = io("https://chatty-server-xy3z.onrender.com");
+export const socket = io(API_BASE_URL);
